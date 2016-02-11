e8319a72811394079c5ab2481fa49c8abd5f946b
junkai <junkai.lu@yahoo.com>
2016-02-11 11:12:31 -0800

    Update sensortag_funcs.py

--- sensortag_funcs.py
+++ sensortag_funcs.py
@@ -2,10 +2,10 @@
 # http://processors.wiki.ti.com/index.php/CC2650_SensorTag_User's_Guide
 def calcTmp(ambT, objT):
     SCALE_LSB = 0.03125
-	it = int(objT >> 2)
-	tObj = float(it)*SCALE_LSB
-	it = int(ambT >> 2)
-	m_tmpAmb = float(it)*SCALE_LSB
+    it = int(objT >> 2)
+    tObj = float(it)*SCALE_LSB
+    it = int(ambT >> 2)
+    m_tmpAmb = float(it)*SCALE_LSB
     return (m_tmpAmb, tObj)
 
 def calcHum(rawT, rawH):
@@ -13,13 +13,13 @@ def calcHum(rawT, rawH):
     hum = (float(rawH)/65536) * 100  # [%RH]
     return (temp, hum)
 
-def calcBaro(rawPr)
+def calcBaro(rawPr):
     pr = rawPr/100.0
     return (pr)
 
 def calcLight(rawL):
     m = rawL & 0x0FFF
-	e = (rawL & 0xF000) >> 12
-	return (m*(0.01*pow(2.0,e)))
+    e = (rawL & 0xF000) >> 12
+    return (m*(0.01*pow(2.0,e)))
 	
 	
